447f52b7a406380c5d7e70db927b0d367cae56ba
ssfarahani2008-collab <s.s.farahani2008@gmail.com>
2026-01-02 21:56:58 +0330

    Add files via upload

---
+++ operator/script.js
@@ -0,0 +1,29 @@
+const display = document.getElementById("display")
+const buttons=document.querySelectorAll(".button")
+let string = ""
+
+
+buttons.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        if (e.target.innerHTML == 'DEL') {
+            string = string.substring(0, string.length - 1);
+            display.value = string;
+        } else if (e.target.innerHTML == 'AC') {
+            string = '';
+            display.value = string;
+        } else if (e.target.innerHTML == '=') {
+            try {
+                string = eval(string);
+                display.value = string;
+            } catch (error) {
+                display.value = 'Error';
+            }
+        } else {
+            string += e.target.innerHTML;
+            display.value = string;
+        }
+    });
+});
+
+    
+
